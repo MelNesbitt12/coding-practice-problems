c658ff5bbda567470c9a556a1a552bc273302197
MelNesbitt12 <mel.nalband@gmail.com>
2020-11-02 16:26:59 -0500

    Completed OOP review challenges

---
+++ data-structures/6-constructor-create.js
@@ -0,0 +1,12 @@
+// Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
+
+function Dog() {
+  this.name = "Karen";
+  this.color = "black";
+  this.numLegs = 4;
+}
+
+let hound = new Dog()
+hound.name = "Coco"
+hound.color = "brown"
+hound.numLegs = 4
