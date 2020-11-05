eceec0f9d4d6c2e76cd9a951ecab760acbac9034
MelNesbitt12 <mel.nalband@gmail.com>
2020-11-05 16:33:23 -0500

    6 basic functional programming problems

---
+++ functional-programming/dependecies.js
@@ -0,0 +1,10 @@
+// The global variable
+// Write the incrementer function so it takes an argument, and then returns a result after increasing the value by one.
+
+var fixedValue = 4;
+
+function incrementer (fixedValue) {
+  return fixedValue + 1
+}
+
+incrementer()
---
+++ functional-programming/global-variable.js
@@ -0,0 +1,13 @@
+// The global variable
+//Fill in the code for the function incrementer so it returns the value of the global variable fixedValue increased by one.
+
+var fixedValue = 4;
+
+function incrementer () {
+
+   let newValue = fixedValue + 1
+   return newValue
+
+}
+
+incrementer()
